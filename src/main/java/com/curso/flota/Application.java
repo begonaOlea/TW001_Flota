c73c46d67d2f8cd3b057589fd65e43050f7b3334
begonaOlea <bolea@programia.es>
2021-10-25 13:32:02 +0200

    vehiculo

---
+++ src/main/java/com/curso/flota/Application.java
@@ -0,0 +1,34 @@
+package com.curso.flota;
+
+import com.curso.flota.dominio.Vehiculo2;
+
+public class Application
+{
+	public static void main( String[] args ){
+		
+		System.out.println("-- prueba clase Vehiculo --");
+		
+		Vehiculo2 v = new Vehiculo2("B111",200.0);
+		
+		System.out.println("cargo  100kg");
+		if ( ! v.cargar(100) ) {
+			System.out.println("No pudo cargar 100");
+		}
+		System.out.println("carga actual " + v.getCargaActual());
+		System.out.println("numero cajas " + v.getNumeroCajas());
+		
+		System.out.println("cargo  120kg");
+		if ( ! v.cargar(120) ) {
+			System.out.println("No pudo cargar 120");
+		}
+		System.out.println("carga actual " + v.getCargaActual());
+		System.out.println("numero cajas " + v.getNumeroCajas());
+		
+		
+	}
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/com/curso/flota/dominio/Vehiculo2.java
@@ -0,0 +1,80 @@
+package com.curso.flota.dominio;
+
+/**
+ * Clase que representa un camion.......
+ * 
+ * @author begonaolea
+ * @version 1
+ */
+public class Vehiculo2 {
+	
+	//atributos
+	private double cargaMaxima;
+	private String matricula;
+	private double cargaActual = 0.0;
+	private int numeroCajas = 0;
+
+	//constructores
+	public  Vehiculo2(String matricula, double cargaMaxima) {
+		super();
+		//validar
+		if(matricula == null) {
+			this.matricula = "";
+		}else {
+			this.matricula = matricula;
+		}
+		if(cargaMaxima <= 100) {
+			this.cargaMaxima = 100;
+		}else {
+			this.cargaMaxima = cargaMaxima;
+		}
+		
+	} //fin constructor
+	
+
+	//métodos
+		
+	public double getCargaMaxima() {
+		return this.cargaMaxima;
+	}
+
+	
+	public String getMatricula() {
+		return matricula;
+	}
+
+	
+	public double getCargaActual() {
+		return cargaActual;
+	}
+
+
+	public int getNumeroCajas() {
+		return numeroCajas;
+	}
+
+    /**
+     * Método que carga una caja (<b>peso en kg</b>) en el camión
+     * Si el peso de la caja supera la carga permitida 
+     * teniendo en cuenta la carga actual, no carga la caja
+     * y devuelve false
+     * EJempl:
+     *     ....
+     * 
+     * @param peso  double con peso de la caja a cargar en kg
+     *              el peso debe ser positivo
+     * @return  boolean que es true pudo carga y false sino
+     */
+	public boolean cargar(double peso) {
+		  if(peso <= 0) {
+			  return false;
+		  }
+		  if( cargaMaxima <  (peso + cargaActual)) {
+			  return false;
+		  }
+		this.cargaActual += peso;
+		this.numeroCajas ++;
+		return true; 
+	}
+
+} //fin clase 
